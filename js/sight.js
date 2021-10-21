6ea29c74fee1132238fefc599bcfca31ae199620
youngmikec <michaelozor15@gmail.com>
2021-10-21 04:18:48 +0100

    created an svg library

--- js/sight.js
+++ js/sight.js
@@ -2,7 +2,7 @@ class SVGElement {
     constructor(type){
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
-        this.node = document.createElementNS(type);
+        this.node = document.createElementNS(this.namespace, this.type);
         return this;
     }
 
